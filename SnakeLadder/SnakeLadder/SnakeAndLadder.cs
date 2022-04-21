58de03fc9a50a363aac827254d59a497a323b14c
Viraj <jadhavviraj50@gmail.com>
2022-04-21 17:44:09 +0530

    [Viraj]-Added UC2SnakeLadder File

--- SnakeLadder/SnakeLadder/SnakeAndLadder.cs
+++ SnakeLadder/SnakeLadder/SnakeAndLadder.cs
@@ -5,14 +5,14 @@ using System.Text;
 
 namespace SnakeLadder
 {
-    //UC1
+    //UC1 Single player at start position 0
     public class SnakeAndLadder
     {
 
         private int position;
         private bool winCheck;
         private int totalDieRoll;
-        
+
         static Random random = new Random();
 
         public SnakeAndLadder()
@@ -27,6 +27,20 @@ namespace SnakeLadder
             Console.WriteLine("Position: " + position);
         }
 
-       
+        //UC2 The Player rolls the die
+
+        public void RollDie()
+        {
+            totalDieRoll++;
+            int chkContinue = 0;
+            int roll = random.Next(1, 7);
+            Console.WriteLine("\nRolled: " + roll);
+            if (winCheck is false)
+                chkContinue = MovePlayer(roll);
+            if (winCheck is false && chkContinue == 1)
+                Option(roll);
+            Describe();
+        }
     }
+
 }
