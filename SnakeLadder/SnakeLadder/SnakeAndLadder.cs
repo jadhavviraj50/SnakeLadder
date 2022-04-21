8c83aabb2594cd68f2a911e39c9f9ef10d11cc9a
Viraj <jadhavviraj50@gmail.com>
2022-04-21 18:07:01 +0530

    [Viraj]-Added UC4SnakeLadder File

--- SnakeLadder/SnakeLadder/SnakeAndLadder.cs
+++ SnakeLadder/SnakeLadder/SnakeAndLadder.cs
@@ -41,6 +41,61 @@ namespace SnakeLadder
                 Option(roll);
             Describe();
         }
+
+        //UC3 Player then checks for the option
+
+        private void Option(int roll)
+        {
+            int option = random.Next(0, 3);
+            switch (option)
+            {
+                case 0:
+                    Console.WriteLine("No Play");
+                    break;
+                case 1:
+                    Ladder(roll);
+                    break;
+                case 2:
+                    Snake(roll);
+                    break;
+                default:
+                    Console.WriteLine("Error!");
+                    break;
+            }
+        }
+
+        private void Snake(int roll)
+        {
+            Console.WriteLine("OOPS! Snake Trap!");
+            Console.WriteLine("Going down by" + roll);
+            MovePlayer(-roll);
+        }
+
+        private void Ladder(int roll)
+        {
+            Console.WriteLine("Yay ! & Ladder!");
+            Console.WriteLine("Going up by " + roll);
+            MovePlayer(roll);
+        }
+
+        //UC4 Position moves below 0 then restart
+        private int CheckBoundary(int displacement)
+        {
+            if (position < 0)
+            {
+                position = 0;
+                return 0;
+            }
+            else if (position > 100)
+            {
+                position -= displacement;
+                Console.WriteLine("Oops! You shot past position 100");
+                Console.WriteLine("You are moved back to previous position");
+                return 0;
+            }
+            else
+                return 1;
+        }
     }
 
 }
