2a41f2f6a95615743c632fb6470a2f0db6a3ac05
Viraj <jadhavviraj50@gmail.com>
2022-04-21 17:25:09 +0530

    [Viraj]-Added UC1SnakeLadder File

---
+++ SnakeLadder/SnakeLadder/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace SnakeLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Snake and Ladder!");
+            SnakeAndLadder player1 = new SnakeAndLadder();
+            SnakeAndLadder player2 = new SnakeAndLadder();
+            Console.WriteLine("Player 1: ");
+            player1.Describe();
+            Console.WriteLine("Player 2: ");
+            player2.Describe();
+            player1.PlayTillEnd(player2);
+        }
+    }
+}
---
+++ SnakeLadder/SnakeLadder/SnakeAndLadder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace SnakeLadder
+{
+    //UC1
+    public class SnakeAndLadder
+    {
+
+        private int position;
+        private bool winCheck;
+        private int totalDieRoll;
+        
+        static Random random = new Random();
+
+        public SnakeAndLadder()
+        {
+            position = 0;
+            winCheck = false;
+            totalDieRoll = 0;
+        }
+
+        public void Describe()
+        {
+            Console.WriteLine("Position: " + position);
+        }
+
+       
+    }
+}
